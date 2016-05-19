54c06023d160261b416dc49e64a49a346c55c7c8
Alexandre Cyro <cyro.alex@gmail.com>
2016-05-18 21:48:06 -0300

    corrigido tpl no resultGallery fotografico

--- app/view/fotografico/ResultGallery.js
+++ app/view/fotografico/ResultGallery.js
@@ -26,20 +26,18 @@ Ext.define('ArqAdmin.view.fotografico.ResultGallery', {
             overItemCls: 'x-item-over',
             tpl: [
                 '<tpl for=".">',
-                    '<tpl if="!Ext.isEmpty(values.imagem_original)">',
-                        '<div class="thumb-wrap thumb-wrap-medium"">',
-                            '<div class="thumb">{[this.getImage(values.id)]}',
-                                '<div class="expand-img">',
-                                    '<a href="#" id="img-{[values.id]}"><span class="icon-expand" style="font-family:icomoon;"> Visualizar</span></a>',
-                                '</div>',
+                    '<div class="thumb-wrap thumb-wrap-medium"">',
+                        '<div class="thumb">{[this.getImage(values)]}',
+                            '<div class="expand-img">',
+                                '<a href="#" id="img-{[values.id]}"><span class="icon-expand" style="font-family:icomoon;"> Visualizar</span></a>',
                             '</div>',
                         '</div>',
-                    '</tpl>',
+                    '</div>',
                 '</tpl>',
                 {
-                    getImage: function (id) {
-                        if (!Ext.isEmpty(id)) {
-                            var imgPath = ArqAdmin.config.Runtime.getImagesFotografico() + id + '/320';
+                    getImage: function (values) {
+                        if (!Ext.isEmpty(values.id) && !Ext.isEmpty(values.imagem_original)) {
+                            var imgPath = ArqAdmin.config.Runtime.getImagesFotografico() + values.id + '/320';
                             return '<img src="' + imgPath + '" onerror="this.src=\'resources/ico/no-image-75.png\';">';
                         } else {
                             return '<img src="resources/ico/no-image-75.png" >';
